5a2fed769315d762750291c688fcda3ce51cb677
ShCodeNik <advert31337@yandex.ru>
2019-08-27 00:09:16 +0300

    comments model migrations

--- src/news/models.py
+++ src/news/models.py
@@ -1,4 +1,6 @@
 from src import db
+from datetime import datetime
+
 
 class News(db.Model):
     __tablename__='news_test'
@@ -10,3 +12,20 @@ class News(db.Model):
 
     def __repr__(self):
         return 'News {} {}'.format(self.title, self.url)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    news_id = db.Column(db.Integer,
+                        db.ForeignKey('news_test.id', ondelete='CASCADE'),
+                        index=True)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        index=True
+    )
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
